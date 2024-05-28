20193cb29e924fd1a1841338f5833a2796dc0a89
Jason Rodrigues <jason_rodrigues@ucsb.edu>
2024-05-27 21:32:57 -0700

    jr user dropdown

---
+++ frontend/src/main/components/Users/UserDropdown.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+import {useFormContext} from "react-hook-form";
+
+const UserDropdown = ({users = [], testId}) => {
+    const {
+        register,
+        formState: {errors}
+    } = useFormContext();
+
+    return (<Form.Group>
+        <Form.Label htmlFor="users">User</Form.Label>
+        <Form.Control data-testid={`${testId}-users`} id="users"  as="select"
+          isInvalid={Boolean(errors.users)}
+          {...register("users", {required: true, minLength: 2})}
+        >
+            <option value=""></option>
+            {users.map(function (object, i) {
+                const key = `${testId}-option-${i}`
+                return(
+                    <option key={key} data-testid={key} value={object.name}>
+                        {object.name}
+                    </option>
+                );
+            })}
+        </Form.Control>
+        <Form.Control.Feedback type="invalid">
+            {errors.users && 'User is required. '}
+        </Form.Control.Feedback>
+    </Form.Group>);
+}
+
+export default UserDropdown;
---
+++ frontend/src/stories/components/Users/UserDropdown.stories.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import UserDropdown from "../../../main/components/Users/UserDropdown";
+import {schoolsFixtures} from "../../../fixtures/schoolsFixtures";
+// import {usersFixtures} from "../../../fixtures/usersFixtures";
+import {Button, Form} from "react-bootstrap";
+import {FormProvider, useForm} from "react-hook-form";
+
+export default {
+    title: "components/Users/UserDropdown",
+    component: UserDropdown
+}
+
+const Template = (args) => {
+    return(
+        <ModelForm {...args} />
+    );
+}
+
+export const Filled = Template.bind({});
+
+Filled.args={
+    users: schoolsFixtures.threeSchools,
+    submitAction: (data) => {
+        console.log("Submit was clicked with data: ", data);
+        window.alert("Submit was clicked with data: " + JSON.stringify(data));
+    }
+}
+
+export const Preselected = Template.bind({});
+
+Preselected.args={
+    users: schoolsFixtures.threeSchools,
+    initialContents: {"user":11111 },
+    submitAction: (data) => {
+        console.log("Submit was clicked with data: ", data);
+        window.alert("Submit was clicked with data: " + JSON.stringify(data));
+    }
+}
+
+
+const ModelForm = ({initialContents, submitAction, users}) =>{
+    const formState = useForm({defaultValues: initialContents || {},});
+    const {
+        handleSubmit
+    } = formState;
+    return(
+        <FormProvider {...formState}>
+            <Form onSubmit={handleSubmit(submitAction)}>
+                <UserDropdown users={users} testId="UserDropdownExample" />
+                <Button
+                    type="submit"
+                    data-testid="UserDropdownExample-submit"
+                >
+                    Submit
+                </Button>
+            </Form>
+        </FormProvider>
+    )
+}
